0da3daf042a2d0710b685a80aa5e6fd83e7ee961
bullfin4 <63576810+bullfin4@users.noreply.github.com>
2020-04-13 23:01:55 +0800

    az

---
+++ js/style.js
@@ -0,0 +1,21 @@
+$('.wrapper').addClass('loaded');
+
+$(function () {
+	jQuery.scrollSpeed(100, 1600, 'easeOutCubic');
+})
+
+$('.icon-menu').click(function (event) {
+	$(this).toggleClass('active');
+	$('menu__body').toggleClass('active');
+	$('body').toggleClass('lock');
+}
+)
+
+function ibg() {
+	$.each($('.ibg'), function (index, val) {
+		if ($(this).find('img').length > 0) {
+			$(this).css('background-image', 'url("' + $(this).find('img').attr('src') + '")');
+		}
+	});
+}
+ibg();
